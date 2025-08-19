0d9944462f861ceb40322eccc03633aee885930b
aleqsio <mikucki@gmail.com>
2025-08-19 13:45:30 +0200

    [file-system] Improve docs

--- packages/expo-file-system/src/ExpoFileSystem.types.ts
+++ packages/expo-file-system/src/ExpoFileSystem.types.ts
@@ -91,10 +91,10 @@ export declare class Directory {
   size: number | null;
 
   /**
-   * @platform android
    * A static method that opens a file picker to select a directory.
    * @param initialUri An optional uri pointing to an initial folder on which the directory picker is opened.
    * @returns a `Directory` instance. The underlying uri will be a content uri on Android.
+   * @platform android
    */
   static pickDirectoryAsync(initialUri?: string): Promise<Directory>;
 }
@@ -232,11 +232,11 @@ export declare class File {
   ): Promise<File>;
 
   /**
-   * @platform android
    * A static method that opens a file picker to select a single file of specified type.
    * @param initialUri An optional uri pointing to an initial folder on which the file picker is opened.
    * @param mimeType A mime type that is used to filter out files that can be picked out.
    * @returns a `File` instance.
+   * @platform android
    */
   static pickFileAsync(initialUri?: string, mimeType?: string): Promise<File>;
 
